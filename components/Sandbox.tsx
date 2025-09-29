e8d1ac1dcfb272c8e90b96ec123aee5238891de2
Aldiar Dauletuly <aldidauletuly@gmail.com>
2025-09-29 22:18:53 +0500

    playing in the sandbox

--- components/Sandbox.tsx
+++ components/Sandbox.tsx
@@ -5,7 +5,7 @@ const fontClassName = fonts[2].font.className;
 
 const Sandbox = () => {
   return (
-    <div className='relative h-96 w-full'>
+    <div className='relative h-96 w-full bg-white'>
       <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  p-8'>
         <p className={clsx(fontClassName, 'text-9xl text-pink-600')}>出</p>
       </div>

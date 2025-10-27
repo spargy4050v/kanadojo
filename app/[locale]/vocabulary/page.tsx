2e608303827f647f0c5ebdb5033d5035fd7d53c8
てんとう虫 <reservecrate@gmail.com>
2025-10-27 22:49:44 +0500

    fixing vercel deployment

--- app/[locale]/vocabulary/page.tsx
+++ app/[locale]/vocabulary/page.tsx
@@ -1,29 +1,24 @@
 import type { Metadata } from 'next';
 import DojoMenu from '@/components/reusable/Menu/DojoMenu';
 
-// export const metadata: Metadata = {
-//   title: 'KanaDojo: Vocabulary',
-//   description:
-//     'The vocabulary dojo is the place where you can learn and practice the words and vocabulary used in day-to-day Japanese.',
-//   openGraph: {
-//     title: 'KanaDojo: Vocabulary',
-//     description:
-//       'The vocabulary dojo is the place where you can learn and practice the words and vocabulary used in day-to-day Japanese.',
-//     url: 'https://kanadojo.com/vocabulary',
-//     type: 'website',
-//     locale: 'en_US',
-//   },
-// };
-
-export const metadata = {
-  title: "Vocabulary",
+export const metadata: Metadata = {
+  title: 'KanaDojo: Vocabulary',
+  description:
+    'The vocabulary dojo is the place where you can learn and practice the words and vocabulary used in day-to-day Japanese.',
+  openGraph: {
+    title: 'KanaDojo: Vocabulary',
+    description:
+      'The vocabulary dojo is the place where you can learn and practice the words and vocabulary used in day-to-day Japanese.',
+    url: 'https://kanadojo.com/vocabulary',
+    type: 'website',
+    locale: 'en_US'
+  }
 };
 
 export const viewport = {
-  width: "device-width",
-  initialScale: 1,
+  width: 'device-width',
+  initialScale: 1
 };
-
 
 export default function VocabPage() {
   return <DojoMenu />;
